859e26cf2c4a19cbaa1746f0ef3fd6368acf5971
PatGeorges <128002889+PatGeorges@users.noreply.github.com>
2024-07-02 06:59:45 -0400

    Delete app_html.py

--- app_html.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.header("Data Science Students in my API6339-2024 course")
-
-HtmlFile = open("studentsLinkedIn.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
-components.html(source_code, height = 2000, width= 2000)
